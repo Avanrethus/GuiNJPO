901261a15f286b320a835e7b56bce366bc1661fc
Avanrethus <kazuhi13@gmail.com>
2016-11-27 15:30:30 +0100

    Utworzyłem ciało klasy Pesel. Dodałem akcje do Buttona.
    Klasa ta ma za zadanie spawdzać poprawność wporwadzonego nr PESEL.

--- GUI/src/PESEL/Pesel.java
+++ GUI/src/PESEL/Pesel.java
@@ -1,14 +1,136 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package PESEL;
-
-/**
- *
- * @author Windows
- */
 public class Pesel {
+    private byte PESEL[] = new byte[11];
+    private boolean poprawny = false;
     
+    public Pesel(String numerPESEL){
+        if (numerPESEL.length()!=11){
+            poprawny = false;
+        }
+        else{
+            for (int i = 0; i<numerPESEL.length(); i++){
+                PESEL[i] = Byte.parseByte(numerPESEL.substring(i,i+1));
+            }
+            if(spSuma() && spMies() && spDzien()){
+                poprawny = true;
+            }
+            else
+                poprawny = false;
+        }
+    }
+    
+
+    public boolean poprawny(){
+        return poprawny;
+    }
+    public int getRok(){
+        int rok;
+        int mies;
+        rok = 10* PESEL[0];
+        rok = rok + PESEL[1];
+        mies = 10* PESEL[2];
+        mies = mies + PESEL[3];
+        if(mies > 80 && mies <93){
+            rok = rok + 1800;
+        }
+        else if (mies > 0 && mies < 13){
+            rok = rok + 1900;
+        }
+        else if (mies > 20 && mies < 33){
+            rok = rok + 2000;
+        } 
+        else if (mies > 40 && mies < 53){
+            rok = rok + 2100;
+        }
+        else if (mies > 60 && mies < 73){
+            rok = rok + 2200;
+        }
+        return rok;
+    }
+    
+    public int getMiesac(){
+        int mies;
+        mies = 10 * PESEL[2];
+        mies = mies + PESEL[3];
+        if(mies > 80 && mies <93){
+            mies = mies - 80;
+        }
+        else if(mies > 20 && mies < 33){
+            mies = mies - 20;
+        }
+        else if(mies > 40 && mies < 53){
+            mies = mies - 40;
+        }
+        else if(mies > 60 && mies < 73){
+            mies = mies - 60;
+        }
+        return mies;
+    }
+    public int getDzien(){
+        int dzien;
+        dzien = 10* PESEL[4];
+        dzien = dzien + PESEL[5];
+        return dzien;
+    }
+    public char getPlec(){
+        if (poprawny){
+            if(PESEL[9] % 2 == 1){
+                return 'M';
+            }
+            else{
+                return 'K';
+            }
+        }
+        else 
+            return 'N';
+    }
+    
+    public boolean spSuma(){
+        int suma = 1* PESEL[0] + 3* PESEL[1] + 7* PESEL[2] + 9* PESEL[3]+
+                   1* PESEL[4] + 3* PESEL[5] + 7* PESEL[6] + 9* PESEL[7]+
+                   1* PESEL[8] + 3* PESEL[9];
+        suma = suma % 10;
+        suma = 10 - suma;
+        suma = suma % 10;
+        if(suma == PESEL[10]){
+            return true;
+        }
+        else
+            return false;
+    }
+    public boolean spMies(){
+        int mies = getMiesac();
+        if (mies > 0 && mies <13){
+            return true;
+        }
+        else
+            return false;
+    }
+    public boolean rokPrzestepny(int rok){
+        if (rok % 4 == 0 && rok % 100 != 0 || rok % 400 == 0){
+            return true;
+        }
+        else 
+            return false;
+    }
+    public boolean spDzien(){
+        int rok = getRok();
+        int mies = getMiesac();
+        int dzien = getDzien();
+        if((dzien > 0 && dzien < 32) && 
+                (mies == 1 || mies == 3 || mies == 5||
+                mies == 7 || mies == 8 || mies == 10 || mies == 12)){
+            return true;
+        }
+        else if((dzien > 0 && dzien < 31) && 
+                (mies == 4 || mies == 6 || mies == 9 || mies == 11)){
+            return true;
+        }
+        else if((dzien > 0 && dzien < 30 && rokPrzestepny(rok)) ||
+                (dzien > 0 && dzien < 29 && !rokPrzestepny(rok))){
+            return true;
+        }
+        else
+            return false;
+    }
 }
--- GUI/src/PESEL/PoprawnoscPeselu.java
+++ GUI/src/PESEL/PoprawnoscPeselu.java
@@ -5,6 +5,8 @@
  */
 package PESEL;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author Windows
@@ -27,21 +29,67 @@ public class PoprawnoscPeselu extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jTextField1 = new javax.swing.JTextField();
+        jLabel1 = new javax.swing.JLabel();
+        jButton1 = new javax.swing.JButton();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jTextField1.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+
+        jLabel1.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+        jLabel1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel1.setText("Wprowadź numer PESEL");
+
+        jButton1.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+        jButton1.setText("Sprawdź poprawność");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(97, 97, 97)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addComponent(jTextField1)
+                            .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, 203, Short.MAX_VALUE)))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(114, 114, 114)
+                        .addComponent(jButton1)))
+                .addContainerGap(100, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(67, 67, 67)
+                .addComponent(jLabel1)
+                .addGap(18, 18, 18)
+                .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, 56, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addComponent(jButton1)
+                .addContainerGap(99, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        String pesel = jTextField1.getText();
+        Pesel p = new Pesel(pesel);
+        if(p.poprawny()){
+           JOptionPane.showMessageDialog(rootPane, "Pesel poprawny");
+        }
+        else
+           JOptionPane.showMessageDialog(rootPane, "Pesel niepoprawny!");
+        
+    }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -79,5 +127,8 @@ public class PoprawnoscPeselu extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton jButton1;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JTextField jTextField1;
     // End of variables declaration//GEN-END:variables
 }
